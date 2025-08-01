6257851f3730400267731b66b25c7120e380a43f
Hmmzel <tokyo_cs1@hotmail.com>
2025-07-31 21:01:39 -0500

    cambio-server.js

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 const express = require("express");
 const app = express();
-const cors = require("cors"); // 👈 importar cors
-const port = 3001;
+const cors = require("cors");
+
+const port = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: "http://localhost:3001",
-    methods: ["GET", "POST"],
+    origin: "https://mercado-consumo-front.vercel.app", // El dominio del frontend
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
-); // 👈 permitir origen y headers
+);
+
 app.use(express.json());
 
 const consumoRoutes = require("./routes/consumos");
 const usuariosRoutes = require("./routes/usuarios");
 const lecturasRoutes = require("./routes/lecturas");
 
-// Ruta raíz
 app.get("/", (req, res) => {
   res.send("¡Hola desde el backend de Node.js!");
 });
 
-// Importa la ruta consumos
 app.use("/api/consumos", consumoRoutes);
 app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/lecturas", lecturasRoutes);
 
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
