a7dec9c46dfb14747e2e614cb93e287e148999c1
Hmmzel <tokyo_cs1@hotmail.com>
2026-01-16 19:59:32 -0500

    retornarslash

--- routes/retornar.js
+++ routes/retornar.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-router.get("", async (req, res) => {
+router.get("/", async (req, res) => {
   res.status(200).json({
     ok: true,
     message: "Se esta escuchando este servicio"
