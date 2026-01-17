844303500e31f6b9af1febb95ba78616fb3751a5
Hmmzel <tokyo_cs1@hotmail.com>
2026-01-16 19:03:29 -0500

    retonarbd

--- routes/retornar.js
+++ routes/retornar.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-router.get("/resultado", async (req, res) => {
+router.get("api/resultado", async (req, res) => {
   try {
-    resultado = res.status(200);
-    console.log("escuchando" + resultado);
+    resultado = res.status(200).json({});
+    console.log("escuchando");
   } catch (error) {
     console.error("Error al obtener usuarios:", error);
     res.status(500).json({ error: "Error al obtener usuarios" });
