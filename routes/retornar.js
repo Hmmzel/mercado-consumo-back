cdb2ab9bdd153de412743484a8c770845a1a437c
Hmmzel <tokyo_cs1@hotmail.com>
2026-01-16 19:53:03 -0500

    retornarF

--- routes/retornar.js
+++ routes/retornar.js
@@ -2,14 +2,11 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-router.get("api/resultado", async (req, res) => {
-  try {
-    resultado = res.status(200).json({});
-    console.log("escuchando");
-  } catch (error) {
-    console.error("Error al obtener usuarios:", error);
-    res.status(500).json({ error: "Error al obtener usuarios" });
-  }
+router.get("", async (req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: "Se esta escuchando este servicio"
+  })
 });
 
 module.exports = router;
